a3737bae4ecf09ec07a303fc78687e172783b6a3
leekihyn <zkffk602@naver.com>
2017-10-13 11:30:38 +0900

    test

---
+++ Test123/src/dynamic_beat_13/ddd.java
@@ -0,0 +1,19 @@
+package dynamic_beat_13;
+
+public class ddd {
+	public static void main(String[] args){
+		String city ="서울";
+		String country ="";
+		switch(city){
+		case "서울" :
+		case "부산" : country = "한국";
+		case "북경" : country = "중국";
+		case "동경" : country = "일본";
+		break;
+		default : country = "대상없음";
+	
+		}
+	System.out.println(country);
+	}
+}
+	
